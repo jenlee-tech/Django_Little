1644ec9daaaabe55ac3194034c2e18de985f4f48
Jennifer Lee <jenlee.tech@gmail.com>
2024-01-17 22:44:14 -0500

    added unique validator for title field

--- LittlelemonAPI/serializers.py
+++ LittlelemonAPI/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import MenuItem, Category
 from decimal import Decimal
 from .models import Category
+from rest_framework.validators import UniqueValidator
 # another example
 
 
@@ -45,6 +46,15 @@ class MenuItemSerializer(serializers.ModelSerializer):
         #     'price': {'min_value': 2},
         #     'stock': {'source': 'inventory', 'min_value': 0}
         # }
+        extra_kwargs = {
+            'title': {
+                'validators': [
+                    UniqueValidator(
+                        queryset=MenuItem.objects.all()
+                    )
+                ]
+            }
+        }
 
     def calculate_tax(self, product: MenuItem):
         return product.price * Decimal(1.1)
